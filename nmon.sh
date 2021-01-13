83f5507abc8452d376c3483f751b9ccd67a644b7
stakezone <cosmos@stake.zone>
2021-01-13 21:55:51 +0000

    nmon polkadot

---
+++ nmon.sh
@@ -0,0 +1,167 @@
+#!/bin/bash
+
+#set -x
+
+#####    sudo apt -y install jq bc
+#####    sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates && curl -sL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+#####    sudo apt update && sudo apt -y install nodejs
+#####    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - && echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+#####    sudo apt update && sudo apt install yarn
+#####    sudo npm install --save @polkadot/api
+
+#####    CONFIG    ##################################################################################################
+validatoraddress=""       # if left empty no validator checks are performed (simple node)
+socket="ws://127.0.0.1:9944" # websocket to node for js-api, if empty default
+cli="timeout --kill-after=6 5 polkadot-js-api --ws $socket" # js api command, using timeout for preventing deadlocks of the script
+heartbeatiptest="on"      # if set to on the local ip is matched with the received heartbeat data (confirms the local node)
+heartbeatoffset="10"      # the block interval following the expected heartbeat height after that a heartbeat must be received
+logname=""                # a custom log file name can be chosen, if left empty default is nodecheck-<username>.log
+logpath="$(pwd)"          # the directory where the log file is stored, for customization insert path like: /my/path
+logsize=200               # the max number of lines after that the log will be trimmed to reduce its size
+sleep1=1s                 # polls every sleep1 sec
+colorI='\033[0;32m'       # black 30, red 31, green 32, yellow 33, blue 34, magenta 35, cyan 36, white 37
+colorD='\033[0;90m'       # for light color 9 instead of 3
+colorE='\033[0;31m'       #
+colorW='\033[0;33m'       #
+noColor='\033[0m'         # no color
+#####  END CONFIG  ##################################################################################################
+
+apiversion=$($cli --version)
+if [ -z $apiversion ]; then
+   echo "please install the Polkadot JS-API"
+   break
+fi
+
+myip=$(curl -s4 checkip.amazonaws.com)
+
+chainid=$($cli rpc.system.chain | jq -r '.chain')
+specVersion=$($cli query.system.lastRuntimeUpgrade | jq -r '.lastRuntimeUpgrade.specVersion')
+localListenAddresses=$($cli rpc.system.localListenAddresses | jq -r '.localListenAddresses | @tsv')
+#nextKeys=$($cli query.session.nextKeys $validatoraddress | jq -r '.nextKeys | @tsv' )
+
+if [ -z $logname ]; then logname="nmon-${USER}.log"; fi
+logfile="${logpath}/${logname}"
+touch $logfile
+
+echo "log file: ${logfile}"
+echo "js-api version: ${apiversion}"
+echo "polkadot version: ${specVersion}"
+echo "websocket: ${socket}"
+echo "validator address: ${validatoraddress}"
+echo "listen addresses: ${localListenAddresses}"
+echo "local ip: ${myip}"
+echo "chain id: ${chainid}"
+#echo "next keys: ${nextKeys}"
+echo ""
+nloglines=$(wc -l <$logfile)
+if [ $nloglines -gt $logsize ]; then sed -i "1,$(expr $nloglines - $logsize)d" $logfile; fi # the log file is trimmed for logsize
+
+date=$(date --rfc-3339=seconds)
+echo "$date status=scriptstarted chainid=$chainid" >>$logfile
+
+while true; do
+   logentry=""
+   health=$($cli rpc.system.health)
+   peers=$(jq -r '.health.peers' <<<$health)
+   isSyncing=$(jq -r '.health.isSyncing' <<<$health)
+   shouldHavePeers=$(jq -r '.health.shouldHavePeers' <<<$health)
+   if [[ $shouldHavePeers = true ]]; then
+      if [[ $peers -gt 0 ]] && [[ $isSyncing = false ]]; then status="synced"; fi
+      if [[ $isSyncing = true ]]; then status="catchingup"; fi
+   else
+      status="error"
+   fi
+   if [ "$status" != "error" ]; then
+      heightfromnow=$($cli query.timestamp.now | jq -r '.now')
+      #heightHash=$($cli rpc.chain.getBlockHash $height | jq -r '.getBlockHash')
+      #getBlock=$($cli rpc.chain.getBlock $heightHash | jq -r '.getBlock')
+      #heightfromnow=$(jq -r '.block.extrinsics[0].method.args[0]' <<<$getBlock)
+      heightfromnow=$(sed 's/,//g' <<<$heightfromnow)
+      heightfromnow=$(echo "scale=0 ; $heightfromnow / 1000" | bc)
+      syncState=$($cli rpc.system.syncState)
+      height=$(jq -r '.syncState.currentBlock' <<<$syncState)
+      height=$(sed 's/,//g' <<<$height)
+      highestBlock=$(jq -r '.syncState.highestBlock' <<<$syncState)
+      highestBlock=$(sed 's/,//g' <<<$highestBlock)
+      behind=$(expr $highestBlock - $height)
+       now=$(date --rfc-3339=seconds)
+      elapsed=$(expr $(date +%s -d "$now") - $heightfromnow)
+      if [ -n "$validatoraddress" ]; then
+         sessionIndex=$($cli query.session.currentIndex | jq -r '.currentIndex')
+         sessionIndex=$(sed 's/,//g' <<<$sessionIndex)
+         #sessionIndex_=$sessionIndex
+         pctSessionElapsed=$(echo "scale=2 ; 100 * ($highestBlock - ($sessionIndex * 1200)) / 1200" | bc)
+         authoredBlocks=$($cli query.imOnline.authoredBlocks $sessionIndex $validatoraddress | jq -r '.authoredBlocks')
+         #keys=$(jq -r 'to_entries | map_values(.value + { index: .key })' <<<$(polkadot-js-api query.imOnline.keys | jq -r 'map({key: .[]})'))
+         keys=$(jq -r 'to_entries | map_values(.value + { index: .key })' <<<$($cli query.session.validators | jq -r 'map({key: .[]})'))
+         validatorKey=$(jq -r '.[] | select(.key == '\"$validatoraddress\"')' <<<$keys)
+         validatorIndex=$(jq -r '.index' <<<$validatorKey)
+         heartbeatAfter_=$($cli query.imOnline.heartbeatAfter)
+         if [ -n "$heartbeatAfter_" ]; then
+            heartbeatAfter=$(jq -r '.heartbeatAfter' <<<$heartbeatAfter_)
+            heartbeatAfter=$(sed 's/,//g' <<<$heartbeatAfter)
+         fi
+         heartbeatDelta=$(expr $highestBlock - $heartbeatAfter)
+         if [ "$heartbeatDelta" -gt "$heartbeatoffset" ]; then
+            heartbeat=missing
+            receivedHeartbeats=$($cli query.imOnline.receivedHeartbeats $sessionIndex $validatorIndex | jq -r '.receivedHeartbeats')
+            if [ "$receivedHeartbeats" != "null" ]; then
+               heartbeat=ok
+               receivedHeartbeats="$(echo $receivedHeartbeats | xxd -r -p | tr -d '\0')"
+            fi
+            if [ "$heartbeatiptest" == "on" ]; then
+               test=$(grep -c $myip <<<$receivedHeartbeats)
+               if [ "$test" == "0" ]; then heartbeat=missing_ip; fi
+            fi
+            if [ "$authoredBlocks" -gt "0" ]; then
+               heartbeat=ok
+            fi
+         else
+            heartbeat="waiting"
+         fi
+         logentry="session=$sessionIndex authoredBlocks=$authoredBlocks heartbeat=$heartbeat"
+      fi
+      logentry="[$now] status=$status height=$height elapsed=$elapsed behind=$behind peers=$peers $logentry"
+      echo "$logentry" >>$logfile
+   else
+      now=$(date --rfc-3339=seconds)
+      status=error
+      logentry="[$now] status=$status"
+      echo "$logentry" >>$logfile
+   fi
+
+   nloglines=$(wc -l <$logfile)
+   if [ $nloglines -gt $logsize ]; then sed -i '1d' $logfile; fi
+
+   case $status in
+   synced)
+      color=$colorI
+      ;;
+   error)
+      color=$colorE
+      ;;
+   catchingup)
+      color=$colorW
+      ;;
+   *)
+      color=$noColor
+      ;;
+   esac
+   if [ $behind -gt 0 ]; then lagging=yes; else lagging=no; fi
+   case $lagging in
+   yes)
+      color=$colorW
+      ;;
+   esac
+   case $heartbeat in
+   missing | missing_ip)
+      color=$colorW
+      ;;
+   esac
+   
+   logentry="$(sed 's/[^ ]*[\=]/'\\${color}'&'\\${noColor}'/g' <<<$logentry)"
+   echo -e $logentry
+   echo -e "${colorD}sleep $sleep1${noColor}"
+   sleep $sleep1
+done
+
